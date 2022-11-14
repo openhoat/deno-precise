84472223a2b6fbb4f1341f040a1a6646f49e0d49
Olivier Penhoat <openhoat@gmail.com>
2022-11-14 12:18:30 +0100

    [FIX] Windows support

--- demo/deno_deploy.ts
+++ demo/deno_deploy.ts
@@ -1,7 +1,8 @@
-import { fromFileUrl, resolve } from 'https://deno.land/std@0.162.0/path/mod.ts'
+import { dirname, fromFileUrl, resolve } from 'https://deno.land/std@0.162.0/path/mod.ts'
 import { assets, WebServer } from '../mod.ts'
 
-const assetsBaseDir = resolve(fromFileUrl(import.meta.url), '..', 'assets')
+const __dirname = dirname(fromFileUrl(import.meta.url))
+const assetsBaseDir = resolve(__dirname, 'assets')
 const webServer = new WebServer({
   handlers: [
     {
--- demo/sample9.ts
+++ demo/sample9.ts
@@ -1,7 +1,8 @@
-import { fromFileUrl, resolve } from 'https://deno.land/std@0.162.0/path/mod.ts'
-import { WebServer, assets } from '../mod.ts'
+import { dirname, fromFileUrl, resolve } from 'https://deno.land/std@0.162.0/path/mod.ts'
+import { assets, WebServer } from '../mod.ts'
 
-const assetsBaseDir = resolve(fromFileUrl(import.meta.url), '..', 'assets')
+const __dirname = dirname(fromFileUrl(import.meta.url))
+const assetsBaseDir = resolve(__dirname, 'assets')
 const webServer = new WebServer()
 webServer.register(assets({ root: assetsBaseDir }))
 await webServer.start()
