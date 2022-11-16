526641ad5c24a0c531eb2d192bd6ba97ff839976
Olivier Penhoat <openhoat@gmail.com>
2022-11-16 09:33:02 +0100

    [BUILD] Add commitlint hook

---
+++ commitlint.config.js
@@ -0,0 +1,14 @@
+const Configuration = {
+  parserPreset: {
+    parserOpts: {
+      headerCorrespondence: ['type', 'ticket', 'subject'],
+      headerPattern: /^\[(\w+)]( #[\w-]+)? ([A-Z]+[\w\s,;:!?./§*%$£&"#'(\-|`_\\^@)°+=€]*)$/,
+    },
+  },
+  rules: {
+    'subject-empty': [2, 'never'],
+    'type-enum': [2, 'always', ['BUILD', 'DOC', 'FEAT', 'FIX', 'REFACTOR', 'TEST', 'UPGRADE']],
+  },
+}
+
+module.exports = Configuration
