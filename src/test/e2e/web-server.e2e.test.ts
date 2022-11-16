0b4e9ea002c1cd451e7a2d8782cb8c62b36aedce
Olivier Penhoat <openhoat@gmail.com>
2022-11-16 14:46:11 +0100

    [TEST] Improve version import

--- src/test/e2e/web-server.e2e.test.ts
+++ src/test/e2e/web-server.e2e.test.ts
@@ -1,8 +1,7 @@
 import { assertEquals } from '../../../dev_deps/std.ts'
 import { Level, Logger } from '../../../deps/x/optic.ts'
-import { exposeVersion, WebServer } from '../../../mod.ts'
+import { exposeVersion, version, WebServer } from '../../../mod.ts'
 import { description } from '../utils.ts'
-import version from '../../../version.ts'
 
 Deno.test('API server e2e tests', async (t) => {
   const logger = new Logger('test').withMinLogLevel(Level.Critical)
