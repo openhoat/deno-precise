7b90e645d7b05c6d7c98340a71db6da5b2c46204
Olivier Penhoat <openhoat@gmail.com>
2022-11-18 07:06:14 +0100

    [TEST] Use tincan

---
+++ dev_deps/x/tincan.ts
@@ -0,0 +1,1 @@
+export * from 'https://deno.land/x/tincan@1.0.1/mod.ts'
--- src/test/e2e/web-server.e2e.test.ts
+++ src/test/e2e/web-server.e2e.test.ts
@@ -1,39 +1,34 @@
-import { assertEquals } from '../../../dev_deps/std.ts'
 import { Level, Logger } from '../../../deps/x/optic.ts'
 import { exposeVersion, version, WebServer } from '../../../mod.ts'
-import { describe, test } from '../../../dev_deps/x/stej.ts'
+import { describe, expect, it, run } from '../../../dev_deps/x/tincan.ts'
 
-describe('API server e2e tests', async () => {
+describe('API server e2e tests', () => {
   const logger = new Logger('test').withMinLogLevel(Level.Critical)
-  await describe('GET /test', async () => {
-    await test(
-      {
-        given: 'a web server initialized with a simple test request handler',
-        should: 'start, handle the request, respond a simple JSON, then stop',
-      },
-      async () => {
-        const webServer = new WebServer({
-          logger,
-        })
-        const testRoutePath = '/test'
-        const testResponseBody = { ok: true }
-        webServer.setBeforeResponse(exposeVersion)
-        webServer.get(testRoutePath, function testHandler() {
-          return Response.json(testResponseBody)
-        })
-        try {
-          await webServer.start()
-          const response = await fetch(`http://localhost:${webServer.port}${testRoutePath}`)
-          const jsonData = await response.json()
-          assertEquals(jsonData, testResponseBody)
-          const versionHeader = response.headers.get('X-Powered-By')
-          assertEquals(versionHeader, `Precise/${version}`)
-        } finally {
-          if (webServer.started) {
-            await webServer.stop()
-          }
+  describe('GET /test', () => {
+    it('should start, handle the request, respond a simple JSON, then stop, given a web server initialized with a simple test request handler', async () => {
+      const webServer = new WebServer({
+        logger,
+      })
+      const testRoutePath = '/test'
+      const testResponseBody = { ok: true }
+      webServer.setBeforeResponse(exposeVersion)
+      webServer.get(testRoutePath, function testHandler() {
+        return Response.json(testResponseBody)
+      })
+      try {
+        await webServer.start()
+        const response = await fetch(`http://localhost:${webServer.port}${testRoutePath}`)
+        const jsonData = await response.json()
+        expect(jsonData).toEqual(testResponseBody)
+        const versionHeader = response.headers.get('X-Powered-By')
+        expect(versionHeader).toEqual(`Precise/${version}`)
+      } finally {
+        if (webServer.started) {
+          await webServer.stop()
         }
-      },
-    )
+      }
+    })
   })
 })
+
+run()
