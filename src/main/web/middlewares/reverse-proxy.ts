4e1fcbe3182bd83d323b4acbb7f7d2089db2bfc8
Olivier Penhoat <openhoat@gmail.com>
2022-11-28 07:58:34 +0100

    [FEAT] Add reverse proxy middleware

---
+++ src/main/web/middlewares/reverse-proxy.ts
@@ -0,0 +1,45 @@
+import type { BaseWebServerable } from '../../types/web/base-web-server.d.ts'
+import type { RequestHandlerContext, RequestWithRouteParams } from '../../types/web/web-server.d.ts'
+import { ConnInfo } from 'https://deno.land/std@0.162.0/http/server.ts'
+
+type ReverseProxyHandlerOptions = {
+  targetHostname: string | ((req: Request) => string)
+  targetPort: number | ((req: Request) => number)
+}
+
+const reverseProxy: (
+  options: ReverseProxyHandlerOptions,
+) => (
+  this: BaseWebServerable,
+  req: RequestWithRouteParams,
+  context: RequestHandlerContext | ConnInfo,
+) => Promise<Response> = (options) =>
+  function proxyHandler(
+    this: BaseWebServerable,
+    req: Request,
+    context: RequestHandlerContext | ConnInfo,
+  ) {
+    const hostname =
+      typeof options.targetHostname === 'function'
+        ? options.targetHostname(req)
+        : options.targetHostname
+    const port =
+      typeof options.targetPort === 'function' ? options.targetPort(req) : options.targetPort
+    const url = `http://${hostname}:${port}`
+    const connInfo = 'connInfo' in context ? context.connInfo : context
+    const forwardedFor = [
+      (connInfo.remoteAddr as Deno.NetAddr).hostname,
+      ...(req.headers.get('x-forwarded-for') || '').split(','),
+    ]
+      .filter((item) => !!item)
+      .join(',')
+    this.logger.info(`Proxify to ${url}`)
+    return fetch(url, {
+      method: req.method,
+      headers: { ...req.headers, 'x-forwarded-for': forwardedFor },
+      body: req.body,
+    })
+  }
+
+export type { ReverseProxyHandlerOptions }
+export { reverseProxy }
