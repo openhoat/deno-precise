488225e58cb27c012f54e516b4c8e8e099ffc5bd
Olivier Penhoat <openhoat@gmail.com>
2022-11-29 17:31:32 +0100

    [FEAT] Add request worker middleware

---
+++ src/main/types/web/middlewares/request-worker.d.ts
@@ -0,0 +1,25 @@
+import type { ConnInfo } from '../../../deps/std.ts'
+
+export type RawRequest = {
+  body: ArrayBuffer
+  headers: HeadersInit
+  method: string
+  url: string
+}
+
+export type RawResponse = {
+  body: ArrayBuffer
+  headers: HeadersInit
+  status: number
+}
+
+export type RequestMessage = {
+  connInfo: ConnInfo
+  request: RawRequest
+  type: 'request'
+}
+
+export type ResponseMessage = {
+  response: RawResponse
+  type: 'response'
+}
---
+++ src/main/web/middlewares/request-worker.ts
@@ -0,0 +1,71 @@
+import type {
+  RawRequest,
+  RawResponse,
+  ResponseMessage,
+} from '../../types/web/middlewares/request-worker.d.ts'
+import type { RequestHandlerSpec } from '../../types/web/web-server.d.ts'
+import { RequestMessage } from '../../types/web/middlewares/request-worker.d.ts'
+
+const toRawHeaders = (headers: Headers): HeadersInit =>
+  Array.from(headers.entries()).reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {})
+const toRawRequest = async (req: Request): Promise<RawRequest> => {
+  const { method, headers, url } = req
+  const body = req.body && (await req.json())
+  return { body, headers: toRawHeaders(headers), method, url }
+}
+
+const fromRawRequest = (jsonRequest: RawRequest) => {
+  const body = jsonRequest.body && JSON.stringify(jsonRequest.body)
+  return new Request(jsonRequest.url, { ...jsonRequest, body })
+}
+
+const toRawResponse = async (res: Response): Promise<RawResponse> => {
+  const { headers, status } = res
+  const body = await res.arrayBuffer()
+  return { body, headers: toRawHeaders(headers), status }
+}
+
+const fromRawResponse = (jsonRes: RawResponse) => new Response(jsonRes.body, jsonRes)
+
+const noCpus = navigator.hardwareConcurrency
+
+const requestWorker: (options: {
+  concurrency?: number
+  workerUrl: string
+}) => RequestHandlerSpec = (options) => {
+  const { concurrency = noCpus, workerUrl } = options
+  const workers = Array.from(Array(concurrency)).map(
+    (_, index) => new Worker(workerUrl, { name: `request worker #${index + 1}`, type: 'module' }),
+  )
+  let workerIndex = 0
+  const handler: RequestHandlerSpec = {
+    handler: async (req, context) => {
+      const worker = workers[workerIndex]
+      workerIndex = (workerIndex + 1) % workers.length
+      const request = await toRawRequest(req)
+      return new Promise<Response>((resolve) => {
+        worker.onmessage = async (evt) => {
+          const { type } = evt
+          if (type !== 'message') {
+            return
+          }
+          const data: ResponseMessage = evt.data
+          if (data.type === 'response') {
+            const jsonResponse: RawResponse = data.response
+            const response = await fromRawResponse(jsonResponse)
+            resolve(response)
+          }
+        }
+        const message: RequestMessage = {
+          connInfo: context.connInfo,
+          request,
+          type: 'request',
+        }
+        worker.postMessage(message)
+      })
+    },
+  }
+  return handler
+}
+
+export { fromRawRequest, fromRawResponse, requestWorker, toRawHeaders, toRawRequest, toRawResponse }
