eb0d05d898d213ec417aeb51edde27dbe094364a
Olivier Penhoat <openhoat@gmail.com>
2022-11-29 17:19:14 +0100

    [UPGRADE] Upgrade Deno dependencies

--- src/test/deps/std.ts
+++ src/test/deps/std.ts
@@ -1,2 +1,2 @@
-export * from 'https://deno.land/std@0.162.0/node/timers.ts'
-export * from 'https://deno.land/std@0.162.0/testing/mock.ts'
+export * from 'https://deno.land/std@0.166.0/node/timers.ts'
+export * from 'https://deno.land/std@0.166.0/testing/mock.ts'
