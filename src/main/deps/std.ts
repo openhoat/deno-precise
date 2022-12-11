10c286ba5c771bd5ff5fe9892d61b93821685b82
Olivier Penhoat <openhoat@gmail.com>
2022-12-11 11:06:54 +0100

    [UPGRADE] Upgrade deps

--- src/main/deps/std.ts
+++ src/main/deps/std.ts
@@ -1,2 +1,2 @@
-export * from 'https://deno.land/std@0.166.0/http/server.ts'
-export * from 'https://deno.land/std@0.166.0/path/mod.ts'
+export * from 'https://deno.land/std@0.167.0/http/server.ts'
+export * from 'https://deno.land/std@0.167.0/path/mod.ts'
