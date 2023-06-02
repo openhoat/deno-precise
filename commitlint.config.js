37077f0a3e9e755429b843a4913a323f2fc0566a
Olivier Penhoat <openhoat@gmail.com>
2023-06-02 10:27:35 +0200

    [BUILD] Improve project config

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,12 +2,21 @@ const Configuration = {
   parserPreset: {
     parserOpts: {
       headerCorrespondence: ['type', 'ticket', 'subject'],
-      headerPattern: /^\[(\w+)]( #[\w-]+)? ([A-Z]+[\w\s,;:!?./§*%$£&"#'(\-|`_\\^@)°+=€]*)$/,
+      headerPattern:
+        /^\[(\w+)]( #[\w-]+)? ([A-Z]+[\w\s,;:!?./§*%$£&"#'(\-|`_\\^@)°+=€]*)$/,
     },
   },
   rules: {
     'subject-empty': [2, 'never'],
-    'type-enum': [2, 'always', ['BUILD', 'DOC', 'FEAT', 'FIX', 'REFACTOR', 'TEST', 'UPGRADE']],
+    'type-enum': [2, 'always', [
+      'BUILD',
+      'DOC',
+      'FEAT',
+      'FIX',
+      'REFACTOR',
+      'TEST',
+      'UPGRADE',
+    ]],
   },
 }
 
